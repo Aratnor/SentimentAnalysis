8f87f672339536d505094d865cfe506ced960060
Aratnor <tunabolat@anadolu.edu.tr>
2018-12-29 21:30:26 +0300

    Added deneme.txt for FileReader Test case

--- src/test/java/preprocessing/FileHandlerTest.java
+++ src/test/java/preprocessing/FileHandlerTest.java
@@ -1,8 +1,15 @@
 package preprocessing;
 
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileHandlerTest {
+
+    @BeforeAll
+    public void setUp() throws Exception {
+    }
+
 
     @org.junit.jupiter.api.Test
     void readFromFile() {
