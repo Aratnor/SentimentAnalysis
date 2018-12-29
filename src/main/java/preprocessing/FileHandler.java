8b225414bc2ae545915c41c9999cfb905500f502
Aratnor <tunabolat@anadolu.edu.tr>
2018-12-29 21:09:24 +0300

    Added readFromFile method and test class for filehandler

--- src/main/java/preprocessing/FileHandler.java
+++ src/main/java/preprocessing/FileHandler.java
@@ -1,4 +1,26 @@
 package preprocessing;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileHandler {
+    public static ArrayList<String> readFromFile(String path) throws IOException {
+        ArrayList<String> lines = new ArrayList<>();
+        BufferedReader fileReader;
+        try {
+            fileReader = new BufferedReader(new FileReader(path));
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                lines.add(line);
+            }
+            fileReader.close();
+            return lines;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found for reading");
+            throw e;
+        }
+    }
 }
---
+++ src/test/java/preprocessing/FileHandlerTest.java
@@ -0,0 +1,10 @@
+package preprocessing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHandlerTest {
+
+    @org.junit.jupiter.api.Test
+    void readFromFile() {
+    }
+}
